d7e06dc790771ac02046a23028ccf9cb3d74fb3b
Jonathan Fernandes <jonh.mf3@hotmail.com>
2026-09-03 00:30:50 -0300

    conecta API segura ao JSONBin

--- api/ranking.js
+++ api/ranking.js
@@ -1,3 +1,7 @@
+const BIN_ID = "6a8501d8da38895dfef51634"
+
+const API_URL = `https://api.jsonbin.io/v3/b/${BIN_ID}/latest`
+
 export async function GET() {
   const accessKey = process.env.JSONBIN_ACCESS_KEY
 
@@ -8,7 +12,32 @@ export async function GET() {
     )
   }
 
-  return Response.json({
-    mensagem: "A API encontrou a chave com segurança!",
-  })
+  try {
+    const response = await fetch(API_URL, {
+      method: "GET",
+      headers: {
+        "X-Access-Key": accessKey,
+        "X-Bin-Meta": "false"
+      }
+    })
+
+    if (!response.ok) {
+      return Response.json(
+        { erro: "Não foi possível buscar o ranking." },
+        { status: response.status }
+      )
+    }
+
+    const dados = await response.json()
+
+    return Response.json(dados)
+
+  } catch (error) {
+    console.error("Erro ao acessar o JSONBin:", error)
+
+    return Response.json(
+      { erro: "Erro interno do servidor." },
+      { status: 500 }
+    )
+  }
 }
